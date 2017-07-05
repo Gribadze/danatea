4df14214c6c57802b18c5badbe73efde781e0216
Dmitry Fedorov <fedor.dmitry@gmail.com>
2017-07-05 17:14:04 +0300

    activity group style

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,18 @@
-$(() => {
+const highlight = () => {
     const headingLetters = $(".logo-heading span");
     let letterIndex = 0;
-    setInterval(() => {
-        let highlightInt = setInterval(() => {
-            $(".logo-heading span.highlighted").removeClass('highlighted');
-            if (letterIndex === headingLetters.length) {
-                letterIndex = 0;
-                clearInterval(highlightInt);
-            } else {
-                $(`.logo-heading span:nth-child(${++letterIndex}`).addClass('highlighted');
-            }
-        }, 400);
-    }, 10000);
+    let highlightInt = setInterval(() => {
+        $(".logo-heading span.highlighted").removeClass('highlighted');
+        if (letterIndex === headingLetters.length) {
+            letterIndex = 0;
+            clearInterval(highlightInt);
+        } else {
+            $(`.logo-heading span:nth-child(${++letterIndex}`).addClass('highlighted');
+        }
+    }, 400);
+    setTimeout(hightlight, 10000);
+}
+
+$(() => {
+    hightlight();
 });
