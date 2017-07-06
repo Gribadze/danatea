fe6aa6dd30dca1b378383d1ebe83e4c8427790b7
Dmitry Fedorov <fedor.dmitry@gmail.com>
2017-07-06 13:21:24 +0300

    first stable index

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,40 @@
-const highlight = () => {
-    const headingLetters = $(".logo-heading span");
-    let letterIndex = 0;
-    let highlightInt = setInterval(() => {
-        $(".logo-heading span.highlighted").removeClass('highlighted');
-        if (letterIndex === headingLetters.length) {
-            letterIndex = 0;
-            clearInterval(highlightInt);
-        } else {
-            $(`.logo-heading span:nth-child(${++letterIndex}`).addClass('highlighted');
-        }
-    }, 400);
-    setTimeout(hightlight, 10000);
+const highlight = (element) => {
+    element.addClass('highlighted');
+    setTimeout(() => element.removeClass('highlighted'), 800);
+    // let highlightInt = setInterval(() => {
+    //     $(".logo-heading span.highlighted").removeClass('highlighted');
+    //     if (letterIndex === headingLetters.length) {
+    //         letterIndex = 0;
+    //         clearInterval(highlightInt);
+    //     } else {
+    //         $(`.logo-heading span:nth-child(${++letterIndex}`).addClass('highlighted');
+    //     }
+    // }, 400);
+    // setTimeout(hightlight, 10000);
 }
 
 $(() => {
-    hightlight();
+    const logoHeadingLetters = $(".logo-heading span");
+    const brandHeadingLetters = $(".brand-heading span");
+    
+    const loopLogoHeading = () => {
+        for(let i = 1; i <= logoHeadingLetters.length; i++) {
+            setTimeout(() => {
+                highlight($(`.logo-heading span:nth-child(${i})`))
+            }, i*200);
+        }
+    };
+    loopLogoHeading();
+    setInterval(loopLogoHeading, 8000);
+    
+    const loopBrandHeading = () => {
+        for(let i = 1; i <= brandHeadingLetters.length; i++) {
+            setTimeout(() => {
+                highlight($(`.brand-heading span:nth-child(${i})`))
+            }, i*200);
+        }
+    };
+    loopBrandHeading();
+    setInterval(loopBrandHeading, 8000);
+    
 });
